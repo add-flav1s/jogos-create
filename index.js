bb148f9921db09b4727c0745e081acbe57fc9ef4
add-flav1s <aflavia17_@hotmail.com>
2021-09-29 19:53:50 -0300

    Comecando a criar as funcoes do jogo.

---
+++ index.js
@@ -0,0 +1,47 @@
+var jogadorEscolha = 0
+var computadorEscolha = 0
+var ganhador = -1
+
+
+
+function jogar(escolha) {
+    jogadorEscolha = escolha
+
+    computadorEscolha = Math.floor((Math.random() * (3 - 1 + 1))) + 1;
+
+    if ((jogadorEscolha == 1) && (computadorEscolha == 1)) {
+        ganhador = 0
+    } else if ((jogadorEscolha == 1) && (computadorEscolha == 2)) {
+        ganhador = 2
+    } else if ((jogadorEscolha == 1) && (computadorEscolha == 3)) {
+        ganhador = 1
+    } else if ((jogadorEscolha == 2) && (computadorEscolha == 1)) {
+        ganhador = 1
+    } else if ((jogadorEscolha == 2) && (computadorEscolha == 2)) {
+        ganhador = 0
+    } else if ((jogadorEscolha == 2) && (computadorEscolha == 3)) {
+        ganhador = 2
+    } else if ((jogadorEscolha == 3) && (computadorEscolha == 1)) {
+        ganhador = 2
+    } else if ((jogadorEscolha == 3) && (computadorEscolha == 2)) {
+        ganhador = 1
+    } else if ((jogadorEscolha == 3) && (computadorEscolha == 3)) {
+        ganhador = 0
+    }
+
+    document.getElementById("jogador-escolha-" + jogadorEscolha).classList.add('selecionado')
+    document.getElementById("computador-escolha-" + computadorEscolha).classList.add('selecionado')
+
+
+    if (ganhador == 0) {
+        document.getElementById('mensagens').innerHTML = 'Empate'
+    } else if (ganhador == 1) {
+        document.getElementById('mensagens').innerHTML = 'Você ganhou'
+    } else if (ganhador == 2) {
+        document.getElementById('mensagens').innerHTML = 'Computador ganhou'
+    }
+
+
+
+
+}
